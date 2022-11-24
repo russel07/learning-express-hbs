1f5dd8c57415879db0ae79f7cbef810cd1e774cf
Russel Hussain <md.russel.hussain@gmail.com>
2022-11-24 11:44:43 +0600

    db connection updated

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,25 @@
-const mongoose = require("mongoose");
-mongoose.connect(
+//const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
+const client = new MongoClient("mongodb+srv://russel:plAMC71qGKpH5gjU@mytest.hl8n9.mongodb.net/?retryWrites=true&w=majority");
+/*mongoose.connect(
     "mongodb+srv://russel:plAMC71qGKpH5gjU@mytest.hl8n9.mongodb.net/?retryWrites=true&w=majority",
     {
         useNewUrlParser: true
     }
-);
+);*/
 
-const db = mongoose.connection;
+/*const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("Connected to MongoDB database")
+});*/
+
+client.connect(err => {
+    if(err){ console.error(err); return false;}
+    // connection to mongo is successful, listen for requests
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
 });
 
-module.exports = db;
+module.exports = client;
